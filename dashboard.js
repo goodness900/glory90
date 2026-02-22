c404515a770ec31dcfcc39aa76b5ed8eba5f9404
goodness900 <winn909061@gmail.com>
2026-02-22 02:54:26 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,19 +1,18 @@
 import { auth } from "./firebase-config.js";
-import { onAuthStateChanged, signOut } 
-from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-const logoutBtn = document.getElementById("logoutBtn");
 const userEmail = document.getElementById("userEmail");
+const logoutBtn = document.getElementById("logoutBtn");
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, user => {
   if (user) {
     userEmail.innerText = "Logged in as: " + user.email;
   } else {
-    window.location.href = "index.html";
+    window.location.href = "login.html";
   }
 });
 
 logoutBtn.addEventListener("click", async () => {
   await signOut(auth);
-  window.location.href = "index.html";
+  window.location.href = "login.html";
 });
