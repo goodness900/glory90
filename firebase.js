5c448d7e5837c459dcdb1f95bef18eca947315e1
goodness900 <winn909061@gmail.com>
2026-02-23 11:56:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } 
-from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
